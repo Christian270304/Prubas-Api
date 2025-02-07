57e55b215221b3de0c773f2704aa3da61bd3c15d
Christian270304 <147045888+Christian270304@users.noreply.github.com>
2025-02-08 00:23:31 +0100

    refactor: move router initialization inside the authentication function for better encapsulation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 
-const router = Router();
-
 export default (pool) => {
+    const router = Router(); // <-- Mueve el router dentro de la función
+
     // Ruta de autenticación 
     router.post('/login', async (req, res) => {
         const { username, password } = req.body;
@@ -13,10 +13,8 @@ export default (pool) => {
             connection.release();
 
             if (results.length > 0 && results[0].password === password) {
-                // Si las credenciales son correctas, enviar una respuesta exitosa
                 res.status(200).json({ message: 'Login successful' });
             } else {
-                // Si las credenciales son incorrectas, enviar una respuesta de error
                 res.status(401).json({ message: 'Invalid credentials' });
             }
         } catch (error) {
@@ -33,7 +31,6 @@ export default (pool) => {
             const [results] = await connection.execute('INSERT INTO users (username, password) VALUES (?, ?)', [username, password]);
             connection.release();
 
-            // Si la inserción fue exitosa, enviar una respuesta exitosa
             res.status(200).json({ message: 'SignUp successful' });
         } catch (error) {
             res.status(500).json({ message: 'Error al registrar el usuario', error });
